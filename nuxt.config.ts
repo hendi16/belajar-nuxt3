e4d9b00288e67e34e814432fed2d742fdbad9887
hendi16 <politeknikhendi16@gmail.com>
2024-05-15 18:36:56 +0700

    belajar-nuxt3: day1,2,3,4

---
+++ nuxt.config.ts
@@ -0,0 +1,51 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  app: {
+    head: {
+      link: [
+        {
+          rel: "stylesheet",
+          href: "https://cdn.jsdelivr.net/npm/remixicon@4.2.0/fonts/remixicon.css",
+        },
+      ],
+    },
+  },
+  devtools: { enabled: true },
+  modules: ["@nuxtjs/tailwindcss", "@vite-pwa/nuxt"],
+  tailwindcss: {
+    cssPath: "~/assets/css/tailwind.css",
+    configPath: "~/tailwind.config.ts",
+  },
+
+  pwa: {
+    manifest: {
+      name: "SMK Coding",
+      short_name: "SMK Coding",
+      theme_color: "#fff",
+      display: "standalone",
+      orientation: "portrait",
+      scope: "/",
+      start_url: "/",
+      icons: [
+        {
+          src: "images/icons/js-96.png",
+          sizes: "96x96",
+          type: "image/png",
+        },
+        {
+          src: "images/icons/js-128.png",
+          sizes: "128x128",
+          type: "image/png",
+        },
+        {
+          src: "images/icons/js-144.png",
+          sizes: "144x144",
+          type: "image/png",
+        },
+      ],
+    },
+    devOptions: {
+      enabled: true,
+    },
+  },
+});
